309d06022e04876b8d858ffcd030beb64a70ebd2
3bobchen <83627389+3bobchen@users.noreply.github.com>
2023-02-08 16:51:00 +1100

    Metrics

--- pages/metrics/index.tsx
+++ pages/metrics/index.tsx
@@ -28,18 +28,6 @@ export default function Home() {
                     },
                 }}
             >
-                {/*<motion.a*/}
-                {/*    variants={FADE_DOWN_ANIMATION_VARIANTS}*/}
-                {/*    href="https://twitter.com/steventey/status/1613928948915920896"*/}
-                {/*    target="_blank"*/}
-                {/*    rel="noreferrer"*/}
-                {/*    className="mx-auto mb-5 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"*/}
-                {/*>*/}
-                {/*    <Twitter className="h-5 w-5 text-[#1d9bf0]" />*/}
-                {/*    <p className="text-sm font-semibold text-[#1d9bf0]">*/}
-                {/*        Introducing Precedent*/}
-                {/*    </p>*/}
-                {/*</motion.a>*/}
                 <motion.h1
                     className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
                     variants={FADE_DOWN_ANIMATION_VARIANTS}
@@ -54,60 +42,28 @@ export default function Home() {
                         See your trends and wardrobe habits and see the impact your wardrobe has on the planet.
                     </Balancer>
                 </motion.p>
-            {/*    <motion.div*/}
-            {/*        className="mx-auto mt-6 flex items-center justify-center space-x-5"*/}
-            {/*        variants={FADE_DOWN_ANIMATION_VARIANTS}*/}
-            {/*    >*/}
-            {/*        <a*/}
-            {/*            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"*/}
-            {/*            href={DEPLOY_URL}*/}
-            {/*            target="_blank"*/}
-            {/*            rel="noopener noreferrer"*/}
-            {/*        >*/}
-            {/*            <svg*/}
-            {/*                className="h-4 w-4 group-hover:text-black"*/}
-            {/*                viewBox="0 0 24 24"*/}
-            {/*                fill="currentColor"*/}
-            {/*                xmlns="http://www.w3.org/2000/svg"*/}
-            {/*            >*/}
-            {/*                <path*/}
-            {/*                    d="M12 4L20 20H4L12 4Z"*/}
-            {/*                    stroke="currentColor"*/}
-            {/*                    strokeWidth="2"*/}
-            {/*                    strokeLinecap="round"*/}
-            {/*                    strokeLinejoin="round"*/}
-            {/*                />*/}
-            {/*            </svg>*/}
-            {/*            <p>Deploy to Vercel</p>*/}
-            {/*        </a>*/}
-            {/*        <a*/}
-            {/*            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"*/}
-            {/*            href="https://github.com/steven-tey/precedent"*/}
-            {/*            target="_blank"*/}
-            {/*            rel="noopener noreferrer"*/}
-            {/*        >*/}
-            {/*            <Github />*/}
-            {/*            <p>Star on GitHub</p>*/}
-            {/*        </a>*/}
-            {/*    </motion.div>*/}
+
             </motion.div>
             {/* here we are animating with Tailwind instead of Framer Motion because Framer Motion messes up the z-index for child components */}
-            <div className="my-10 grid w-full max-w-screen-xl animate-[slide-down-fade_0.5s_ease-in-out] grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-                {features.map(({ title, description, demo }) => (
-                    <Card
-                        key={title}
-                        title={title}
-                        description={description}
-                        demo={
-                            title === "Beautiful, reusable components" ? (
-                                <ComponentGrid />
-                            ) : (
-                                demo
-                            )
-                        }
-                        large={false}
-                    />
-                ))}
+            {/*<div className="my-10 grid w-full max-w-screen-xl animate-[slide-down-fade_0.5s_ease-in-out] grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">*/}
+            {/*    {features.map(({ title, description, demo }) => (*/}
+            {/*        <Card*/}
+            {/*            key={title}*/}
+            {/*            title={title}*/}
+            {/*            description={description}*/}
+            {/*            demo={*/}
+            {/*                title === "Beautiful, reusable components" ? (*/}
+            {/*                    <ComponentGrid />*/}
+            {/*                ) : (*/}
+            {/*                    demo*/}
+            {/*                )*/}
+            {/*            }*/}
+            {/*            large={false}*/}
+            {/*        />*/}
+            {/*    ))}*/}
+            {/*</div>*/}
+            <div>
+
             </div>
         </Layout>
     );
