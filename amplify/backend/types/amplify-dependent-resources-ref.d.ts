5fcf92b3a4b4b8d83e5c2e578173a6d689008816
Harrison Tedeschi <harrison.tedeschi@accenture.com>
2023-02-09 10:54:26 +1100

    initial amplify setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "bootcampg5f14a0904": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "dev": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
